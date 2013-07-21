0de2c77b4c3a96f7c2e6037b9f8c38a7e73ef40f
Théotime <theotime.loiseau@gmail.com>
2013-07-21 23:10:31 +0200

    First layout for the user description - need to add the real details

--- src/Bestyear/MainBundle/Controller/MainController.php
+++ src/Bestyear/MainBundle/Controller/MainController.php
@@ -10,4 +10,9 @@ class MainController extends Controller
     {
         return $this->render('BestyearMainBundle:Main:index.html.twig', array());
     }
+    
+    public function userAction()
+    {
+        return $this->render('BestyearMainBundle:Main:userView.html.twig', array());
+    }
 }
