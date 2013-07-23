5ff51fa56c82ae33aa3519082f40ca7e58b2f34f
Théotime <theotime.loiseau@gmail.com>
2013-07-23 18:51:31 +0200

    Start to create the security system with users - to be finished + design of the login form

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bestyear\MainBundle\BestyearMainBundle(),
+            new Bestyear\UserBundle\BestyearUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
