4ef33b886487c29eb5613c654f716d5a9e476094
Théotime <theotime.loiseau@gmail.com>
2013-08-02 11:12:53 +0200

    Better Check (JQuery) - ToolTip to explain the errors - Step 5 added

--- src/Bestyear/UserBundle/Resources/public/js/formChecker.js
+++ src/Bestyear/UserBundle/Resources/public/js/formChecker.js
@@ -1,23 +1,54 @@
-function checkEmpty(obj) {
-    if(!obj.val()) {
-        obj.parents('span').addClass('inputError');
-    } else {
-        obj.parents('span').removeClass('inputError');
-    } 
-}
-
 function checkFormOnSubmit() {
-    checkEmpty($('.required'));
+    checkLogin();
+    checkPassword();
 }
 
-$('.required').keyup(function() {
-    checkEmpty($(this));
-});
-
-$('.required').blur(function() {
-    checkEmpty($(this));
-});
-
-$("#submit").click(function () {
+function checkLogin() {
+    error = false;
+    username = $('#username');
+    if (!username.val()) {
+        // If the field is empty
+        error = true;
+    } else if (username.val().length != 8) {
+        // If the username hasn't the right size (8char)
+        error = true;
+    }
     
+    if (error) {
+        username.parents('span').addClass('inputError');
+    } else {
+        username.parents('span').removeClass('inputError');
+    }
+}
+
+function checkPassword() {
+    error = false;
+    password = $('#password');
+    if (!password.val()) {
+        // If the field is empty
+        error = true;
+    }
+    
+    if (error) {
+        password.parents('span').addClass('inputError');
+    } else {
+        password.parents('span').removeClass('inputError');
+    }
+}
+
+$('#username')
+    .keyup(function() {checkLogin();})
+    .blur(function() {checkLogin();});
+    
+$('#password')
+    .keyup(function() {checkPassword();})
+    .blur(function() {checkPassword();});
+
+$('#formLogin').submit(function() {
+    checkFormOnSubmit();
+    if ($('#formLogin').find('.inputError').length > 0) {
+        return false;
+    }
+    return true;
 });
+
--- src/Bestyear/UserBundle/Resources/public/js/register.js
+++ src/Bestyear/UserBundle/Resources/public/js/register.js
@@ -8,7 +8,115 @@ var currentStep = 1;
 var previousStep = null;
 var maximumStep = 1;
 var errors = 0;
-nbStep = 4;
+nbStep = 5;
+
+/*
+ * When enter is pressed, go to the next step
+ */
+ 
+$(document).keypress(function(e) {
+    // If key "Enter" is pressed
+    if (e.which == 13) {
+        if (currentStep < nbStep) {
+            nextStep();
+        } else if (currentStep == nbStep) {
+            submitForm();
+        }
+        
+    }
+});
+
+$(".nextStep").keypress(function(e) {
+    // If the user presses "Enter" while the focus is on the button
+    if (e.which == 13) {
+        nextStep();
+        return false;
+    }
+});
+
+$(".submit").keypress(function(e) {
+    // If the user presses "Enter" while the focus is on the button
+    if (e.which == 13) {
+        submitForm();
+        return false;
+    }
+});
+
+/*
+ * Set the focus on the first input 
+ */
+ 
+$(document).ready(function () {
+    $("#username").focus(); 
+});
+
+/*
+ * Handles the tooltips when there is an error
+ */
+
+var qtip_options = {
+    style: {
+        classes: "qtip-bootstrap",
+        tip: {
+            corner: true,
+            width: 8,
+        },
+    },
+    position: {
+        my: 'bottom center',
+        at: 'top center',
+        target: $(this).next()
+    },
+    show: {
+        event: false,
+        ready: true
+    },
+    hide: {
+        event: false
+    }
+};
+
+function updateErrorMessage(obj) {
+    var errorLogo = obj.next();
+    obj.qtip({
+        style: {
+            classes: "qtip-bootstrap",
+            tip: {
+                corner: true,
+                width: 8,
+            },
+        },
+        position: {
+            my: 'bottom center',
+            at: 'top center',
+            target: errorLogo
+        },
+        show: {
+            event: false,
+            ready: true,
+            solo: true
+        },
+        hide: {
+            event: false
+        }
+    });
+}
+
+$("input").focus(function() {
+    updateErrorMessage($(this));
+});
+
+$("input").keyup(function() {
+    if(!('object' === typeof $(this).data('qtip'))) {
+        updateErrorMessage($(this));
+    }
+});
+
+$("input").blur(function() {
+    $(this).qtip('destroy');
+});
+
+
 
 /*
  * ============================
@@ -79,6 +187,22 @@ $('#postcode2')
     .keyup(function() {checkPostcode2();})
     .blur(function() {checkPostcode2();});
 
+// ********
+// STEP 4 *
+// ********
+    
+$('#email')
+    .keyup(function() {checkMail();})
+    .blur(function() {checkMail();});
+    
+$('#facebook')
+    .keyup(function() {checkURLFacebook();})
+    .blur(function() {checkURLFacebook();});
+    
+$('#twitter')
+    .keyup(function() {checkURLTwitter();})
+    .blur(function() {checkURLTwitter();});
+
 /*
  * ============================
  * Error treatment on submit
@@ -92,54 +216,73 @@ $('#postcode2')
 // Check the length of the username input
 function checkUsername() {
     error = false;
+    message = null;
     username = $('#username');
     if (!username.val()) {
         // If the field is empty
         error = true;
+        message = "Champ vide";
     } else if (username.val().length != 8) {
         // If the username hasn't the right size (8char)
         error = true;
+        message = "8 caractères exactement";
     }
     
     if (error) {
         username.parents('span').addClass('inputError');
+        username.attr("title", message);
     } else {
         username.parents('span').removeClass('inputError');
+        username.attr("title", "");
     }
 }
 
 // Tests on the password
 function checkPassword() {
     error1 = false;
+    message1 = null;
     error2 = false;
+    message2 = null;
     pass1 = $('#password');
     pass2 = $('#password2');
     
     // Test if the input is empty
     if (!pass1.val()) {
         error1 = true;
+        message1 = "Champ vide";
     }
     
     // The password should be 6 char long at least
     if (pass1.val() && pass1.val().length < 6) {
         error1 = true;
+        message1 = "6 caractères min.";
+    }
+    
+    if (!pass2.val()) {
+        error2 = true;
+        message2 = "Champ vide";
     }
 
     // The password and its confirmation should be the same
     if (pass2.val() && pass1.val() != pass2.val()) {
         error2 = true;
+        message2 = "Les mots de passe sont différents";
     }
     
     if (error1) {
         pass1.parents('span').addClass('inputError');
+        pass1.attr("title", message1);
     } else {
         pass1.parents('span').removeClass('inputError');
+        pass1.attr("title", "");
     }
     
     if (error2) {
         pass2.parents('span').addClass('inputError');
+        pass2.attr("title", message2);
     } else {
         pass2.parents('span').removeClass('inputError');
+        pass2.attr("title", "");
     }
 }
 
@@ -149,43 +292,53 @@ function checkPassword() {
 
 function checkGivenName() {
     error = false;
+    message = null;
     givenname = $('#givenname');
     
     // Check if not empty
     if (!givenname.val()) {
+        message = "Champ vide";
         error = true;
     }
     
     if (error) {
         givenname.parents('span').addClass('inputError');
+        givenname.attr("title", message);
     } else {
         givenname.parents('span').removeClass('inputError');
+        givenname.attr("title", "");
     }
 }
 
 function checkFamilyName() {
     error = false;
+    message = null;
     familyname = $('#familyname');
     
     // Check if not empty
     if (!familyname.val()) {
+        message = "Champ vide";
         error = true;
     }
     
     if (error) {
         familyname.parents('span').addClass('inputError');
+        familyname.attr("title", message);
     } else {
         familyname.parents('span').removeClass('inputError');
+        familyname.attr("title", "");
     }
 }
 
 function checkBirthdate() {
     error = false;
+    message = null;
     birthdate = $('#birthdate');
    
     // Correct format
     var matches = birthdate.val().match('^((19|20)[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$')
     if (!matches) {
+        message = "Format invalide (Année-Mois-Jour)";
         error = true;
     } else {
         var birthDay = parseInt(matches[4]);
@@ -199,19 +352,24 @@ function checkBirthdate() {
     var curr_month = d.getMonth()+1;
     var curr_year = d.getFullYear();
     if (birthYear > curr_year-10) {
+        message = "Il faut avoir plus de 10 ans";
         error = true;
     }
     if (birthYear == curr_year-10 && birthMonth > curr_month) {
+        message = "Il faut avoir plus de 10 ans";
         error = true;
     }
     if (birthYear == curr_year-10 && birthMonth == curr_month && birthDay >= curr_date) {
+        message = "Il faut avoir plus de 10 ans";
         error = true;
     }
     
     if (error) {
         birthdate.parents('span').addClass('inputError');
+        birthdate.attr("title", message);
     } else {
         birthdate.parents('span').removeClass('inputError');
+        birthdate.attr("title", "");
     }
 }
 
@@ -221,21 +379,29 @@ function checkBirthdate() {
 
 function checkPostcode1() {
     postcode1 = $('#postcode1');
+    message = null;
     
     if (postcode1.val() && !postcode1.val().match('^[0-9]{5}$')) {
+        message = "Format incorrect (5 chiffres)";
         postcode1.parents('span').addClass('inputError');
+        postcode1.attr("title", message);
     } else if (postcode1.val() && postcode1.val().match('^[0-9]{5}$')) {
         postcode1.parents('span').removeClass('inputError');
+        postcode1.attr("title", "");
     }
 }
 
 function checkPostcode2() {
-    postcode1 = $('#postcode2');
+    postcode2 = $('#postcode2');
+     message = null;
     
-    if (postcode2.val() && !postcode1.val().match('^[0-9]{5}$')) {
+    if (postcode2.val() && !postcode2.val().match('^[0-9]{5}$')) {
+        message = "Format incorrect (5 chiffres)";
         postcode2.parents('span').addClass('inputError');
+        postcode2.attr("title", message);
     } else if (postcode2.val() && postcode2.val().match('^[0-9]{5}$')) {
         postcode2.parents('span').removeClass('inputError');
+        postcode2.attr("title", "");
     }
 }
 
@@ -249,21 +415,29 @@ function checkAddress1onSubmit() {
     if ((streetnumber1.val() || street1.val() || postcode1.val() || city1.val()) && !(streetnumber1.val() && street1.val() && postcode1.val() && city1.val())) {
         if (!streetnumber1.val()) {
             streetnumber1.parents('span').addClass('inputError');
+            streetnumber1.attr("title", "Champ vide");
         }
         if (!street1.val()) {
             street1.parents('span').addClass('inputError');
+            street1.attr("title", "Champ vide");
         }
         if (!postcode1.val()) {
             postcode1.parents('span').addClass('inputError');
+            postcode1.attr("title", "Champ vide");
         }
         if (!city1.val()) {
             city1.parents('span').addClass('inputError');
+            city1.attr("title", "Champ vide");
         }
     } else {
         streetnumber1.parents('span').removeClass('inputError');
         street1.parents('span').removeClass('inputError');
         postcode1.parents('span').removeClass('inputError');
         city1.parents('span').removeClass('inputError');
+        streetnumber1.attr("title", "");
+        street1.attr("title", "");
+        postcode1.attr("title", "");
+        city1.attr("title", "");
     }
 }
 
@@ -277,28 +451,188 @@ function checkAddress2onSubmit() {
     if ((streetnumber2.val() || street2.val() || (postcode2.val() && postcode2.val() != "60200") || (city2.val()) && city2.val() != "Compiègne") && !(streetnumber2.val() && street2.val() && postcode2.val() && city2.val())) {
         if (!streetnumber2.val()) {
             streetnumber2.parents('span').addClass('inputError');
+            streetnumber2.attr("title", "Champ vide");
         }
         if (!street2.val()) {
             street2.parents('span').addClass('inputError');
+            street2.attr("title", "Champ vide");
         }
         if (!postcode2.val()) {
             postcode2.parents('span').addClass('inputError');
+            postcode2.attr("title", "Champ vide");
         }
         if (!city2.val()) {
             city2.parents('span').addClass('inputError');
+            city2.attr("title", "Champ vide");
         }
     } else {
         streetnumber2.parents('span').removeClass('inputError');
         street2.parents('span').removeClass('inputError');
         postcode2.parents('span').removeClass('inputError');
         city2.parents('span').removeClass('inputError');
+        streetnumber2.attr("title", "");
+        street2.attr("title", "");
+        postcode2.attr("title", "");
+        city2.attr("title", "");
+    }
+}
+
+// ********
+// STEP 4 *
+// ********
+
+function checkMail() {
+    error = false;
+    message = null;
+    email = $('#email');
+   
+    // Correct format
+    if (email.val() && !email.val().match('^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$')) {
+        message = "Fomat incorrect";
+        error = true;
+    }
+    
+    if (error) {
+        email.parents('span').addClass('inputError');
+        email.attr("title", message);
+    } else {
+        email.parents('span').removeClass('inputError');
+        email.attr("title", "");
+    }
+}
+
+function checkURLFacebook() {
+    error = false;
+    message = null;
+    urlFacebook = $('#facebook');
+   
+    if (urlFacebook.val() && !urlFacebook.val().match('^(https?:\/\/)?(www\.)?facebook\.com/[a-z.-1-9_]+$')) {
+        message = "URL invalide (ex: www.facebook.com/mon.profil)";
+        error = true;
+    }
+    if (error) {
+        urlFacebook.parents('span').addClass('inputError');
+        urlFacebook.attr("title", message);
+    } else {
+        urlFacebook.parents('span').removeClass('inputError');
+        urlFacebook.attr("title", "");
+    }
+}
+
+function checkURLTwitter() {
+    error = false;
+    message = null;
+    urlTwitter = $('#twitter');
+   
+    if (urlTwitter.val() && !urlTwitter.val().match('^(https?:\/\/)?(www\.)?twitter\.com/[a-zA-Z.-1-9_]+$')) {
+        message = "URL invalide (ex: www.twitter.com/tym-network)";
+        error = true;
+    }
+    if (error) {
+        urlTwitter.parents('span').addClass('inputError');
+        urlTwitter.attr("title", message);
+    } else {
+        urlTwitter.parents('span').removeClass('inputError');
+        urlTwitter.attr("title", "");
+    }
+}
+
+function checkTN05() {
+    message = null;
+    tn05_job = $('#tn05_job');
+    tn05_place = $("#tn05_place");
+    
+    // If only one of the two inputs isn't filled
+    if ((!tn05_job.val() || !tn05_place.val()) && !(!tn05_job.val() && !tn05_place.val())) {
+        message = "Champ vide";
+        if (!tn05_job.val()) {
+            tn05_job.parents('span').addClass('inputError');
+            tn05_job.attr("title", message);
+        } else if (!tn05_place.val()) {
+            tn05_place.parents('span').addClass('inputError');
+            tn05_place.attr("title", message);
+        }
+    } else {
+        tn05_job.parents('span').removeClass('inputError');
+        tn05_place.parents('span').removeClass('inputError');
+        tn05_job.attr("title", "");
+        tn05_place.attr("title", "");
+    }
+}
+
+function checkTN07() {
+    message = null;
+    tn07_job = $('#tn07_job');
+    tn07_place = $("#tn07_place");
+    
+    // If only one of the two inputs isn't filled
+    if ((!tn07_job.val() || !tn07_place.val()) && !(!tn07_job.val() && !tn07_place.val())) {
+        message = "Champ vide";
+        if (!tn07_job.val()) {
+            tn07_job.parents('span').addClass('inputError');
+            tn07_job.attr("title", message);
+        } else if (!tn07_place.val()) {
+            tn07_place.parents('span').addClass('inputError');
+            tn07_place.attr("title", message);
+        }
+    } else {
+        tn07_job.parents('span').removeClass('inputError');
+        tn07_place.parents('span').removeClass('inputError');
+        tn07_job.attr("title", "");
+        tn07_place.attr("title", "");
+    }
+}
+
+function checkTN09() {
+    message = null;
+    tn09_job = $('#tn09_job');
+    tn09_place = $("#tn09_place");
+    
+    // If only one of the two inputs isn't filled
+    if ((!tn09_job.val() || !tn09_place.val()) && !(!tn09_job.val() && !tn09_place.val())) {
+        message = "Champ vide";
+        if (!tn09_job.val()) {
+            tn09_job.parents('span').addClass('inputError');
+            tn09_job.attr("title", message);
+        } else if (!tn09_place.val()) {
+            tn09_place.parents('span').addClass('inputError');
+            tn09_place.attr("title", message);
+        }
+    } else {
+        tn09_job.parents('span').removeClass('inputError');
+        tn09_place.parents('span').removeClass('inputError');
+        tn09_job.attr("title", "");
+        tn09_place.attr("title", "");
+    }
+}
+
+function checkTN10() {
+    message = null;
+    tn10_job = $('#tn10_job');
+    tn10_place = $("#tn10_place");
+    
+    // If only one of the two inputs isn't filled
+    if ((!tn10_job.val() || !tn10_place.val()) && !(!tn10_job.val() && !tn10_place.val())) {
+        message = "Champ vide";
+        if (!tn10_job.val()) {
+            tn10_job.parents('span').addClass('inputError');
+            tn10_job.attr("title", message);
+        } else if (!tn10_place.val()) {
+            tn10_place.parents('span').addClass('inputError');
+            tn10_place.attr("title", message);
+        }
+    } else {
+        tn10_job.parents('span').removeClass('inputError');
+        tn10_place.parents('span').removeClass('inputError');
+        tn10_job.attr("title", "");
+        tn10_place.attr("title", "");
     }
 }
 
 function checkStep(id) {
     switch (id) {
         case 1:
-            // Checking step1
+            // Checking step 1
             checkUsername();
             checkPassword();
             if ($('#step1').find('.inputError').length > 0) {
@@ -309,7 +643,7 @@ function checkStep(id) {
             return true;
             break;
         case 2:
-            // Checking step2
+            // Checking step 2
             checkGivenName();
             checkFamilyName();
             checkBirthdate();
@@ -321,7 +655,7 @@ function checkStep(id) {
             return true;
             break;
         case 3:
-            // Checking step3
+            // Checking step 3
             checkAddress1onSubmit();
             checkAddress2onSubmit();
             checkPostcode1();
@@ -331,6 +665,31 @@ function checkStep(id) {
                 return false;
             }
             $('#bubble3').removeClass('errorBubble');
+            return true;
+            break;
+        case 4:
+            // Checking step 4
+            checkMail();
+            checkURLFacebook();
+            checkURLTwitter();
+            if ($('#step4').find('.inputError').length > 0) {
+                $('#bubble4').addClass('errorBubble');
+                return false;
+            }
+            $('#bubble4').removeClass('errorBubble');
+            return true;
+            break;
+        case 5:
+            // Checking step 5
+            checkTN05();
+            checkTN07();
+            checkTN09();
+            checkTN10();
+            if ($('#step5').find('.inputError').length > 0) {
+                $('#bubble5').addClass('errorBubble');
+                return false;
+            }
+            $('#bubble5').removeClass('errorBubble');
             return true;
             break;
     }
@@ -344,6 +703,10 @@ function checkStep(id) {
 
 $('.nextStep').click(function() {
     nextStep();
+});
+
+$('.submit').click(function() {
+    submitForm();
 });
 
 // When a user clicks on one of the bubble in the left
@@ -365,6 +728,30 @@ function nextStep() {
                 previousBubble.removeClass('current');
             }
         }
+    }
+}
+
+function submitForm() {
+    checkStep(nbStep);
+}
+
+function focusFirstInput(id) {
+    switch (id) {
+        case 1:
+            $("#username").focus(); 
+            break;
+        case 2:
+            $("#givenname").focus(); 
+            break;
+        case 3:
+            $("#streetnumber1").focus(); 
+            break;
+        case 4:
+            $("#phone1").focus(); 
+            break;
+        case 5:
+            $("#tn05_job").focus(); 
+            break;
     }
 }
 
@@ -396,6 +783,6 @@ function loadStep(id) {
             currentDiv
                 .show()
                 .stop()
-                .animate({"opacity": 1}, "slow");
+                .animate({"opacity": 1}, "slow", function() {focusFirstInput(id);});
         });
 }
